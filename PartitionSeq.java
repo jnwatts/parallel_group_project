d6fb797c6b1dbd15bd56183e3d35a793b160e0e7
Josh Watts <jwatts@d3engineering.com>
2013-04-01 23:05:04 -0400

    Sequential program works
    Output needs cleaning/reduction

--- PartitionSeq.java
+++ PartitionSeq.java
@@ -1,6 +1,7 @@
 import java.lang.Math;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Iterator;
 import edu.rit.util.Random;
 
@@ -10,10 +11,12 @@ public class PartitionSeq
 	static int M;
 	static int s;
 
+	static Boolean debug = false;
 	static Random prng;
 
 	static int[] numbers; // Set of numbers to be partitioned
 	static int[] current_arrangement; // Each index corresponds to an index of the numbers array, the value represents the set in which the number is placed
+	static long num_arrangements;
 	static long arrangements_remaining;
 	
 	static int min_score;
@@ -21,6 +24,8 @@ public class PartitionSeq
 
 	public static void main(String[] args)
 	{
+		long t0 = System.currentTimeMillis();
+		
 		if (args.length != 3) {
 			usage();
 			return;
@@ -40,38 +45,53 @@ public class PartitionSeq
 		numbers = new int[N];
 		current_arrangement = new int[N];
 
-		arrangements_remaining = 1L;
+		num_arrangements = 1L;
 		for (int i = 0; i < N; ++i) {
-			arrangements_remaining *= M;
+			num_arrangements *= M;
 		}
+		arrangements_remaining = num_arrangements;
 
 		min_score = Integer.MAX_VALUE;
 		min_arrangements = new LinkedList<int[]>();
 
 		// Generate numbers to operate on and initialize first arrangement
 		for (int i = 0; i < N; ++i) {
-			numbers[i] = i; //prng.nextInt(N);
+			numbers[i] = prng.nextInt(N) - N/2;
 			current_arrangement[i] = 0;
 		}
+		System.out.printf("numbers:\n");
+		dump_array(numbers);
+		System.out.printf("possible arrangements: %d\n", num_arrangements);
 
 		while (arrangements_remaining > 0) {
+			if (debug) {
+				System.out.printf("arrangements_remaining #%d\n", arrangements_remaining);
+				dump_array(current_arrangement);
+			}
+
 			// Score arrangement
 			{
 				int[] sums = new int[M];
 				int score;
 				int min_index = 0;
 				int max_index = 0;
+				int min_sum = Integer.MAX_VALUE;
+				int max_sum = Integer.MIN_VALUE;
 				for (int i = 0; i < N; ++i) {
 					int index = current_arrangement[i];
 					int num = numbers[i];
 					sums[index] += num;
 				}
 				for (int i = 0; i < M; ++i) {
-					int sum = sums[current_arrangement[i]];
-					if (sum < sums[min_index])
+					int sum = sums[i];
+					if (sum < min_sum) {
 						min_index = i;
-					if (sum > sums[max_index])
+						min_sum = sum;
+					}
+					if (sum > max_sum) {
 						max_index = i;
+						max_sum = sum;
+					}
 				}
 				score = sums[max_index] - sums[min_index];
 
@@ -81,6 +101,13 @@ public class PartitionSeq
 						min_score = score;
 					}
 					min_arrangements.add((int[])current_arrangement.clone());
+				}
+				if (debug) {
+					System.out.printf("sums #%d\n", arrangements_remaining);
+					dump_array(sums);
+					System.out.printf("min: %d, max: %d\n", min_index, max_index);
+					System.out.printf("score: %d\n", score);
+					System.out.printf("-\n");
 				}
 			}
 
@@ -97,25 +124,61 @@ public class PartitionSeq
 			--arrangements_remaining;
 		}
 
-		System.out.printf("numbers:");
-		for (int i = 0; i < N; ++i) {
-			System.out.printf("%d,", numbers[i]);
+		if (debug) {
+			System.out.printf("\n");
 		}
-		System.out.printf("\n");
+
 		System.out.printf("min_score: %d\n", min_score);
-		System.out.printf("arrangements:\n");
+		System.out.printf("arrangements with this score: %d\n", min_arrangements.size());
+		//TODO: Sort arrangements and print only the top one
 		Iterator<int[]> iter = min_arrangements.iterator();
 		while (iter.hasNext()) {
 			int[] arrangement = iter.next();
-			for (int i = 0; i < N; ++i) {
-				System.out.printf("%d,", arrangement[i]);
-			}
-			System.out.printf("\n");
+			System.out.println(arrangement_toString(arrangement));
 		}
 		
+		long t1 = System.currentTimeMillis();
+		System.out.printf("%d msecs\n", t1 - t0);
 	}
 
 	public static void usage() {
 		System.err.println("Usage: PartitionSeq <N numbers> <M partitions> <seed>");
+	}
+
+	public static void dump_array(int[] a) {
+		for (int i = 0; i < a.length; ++i) {
+			System.out.printf("%d,", a[i]);
+		}
+		System.out.printf("\n");
+	}
+
+	public static String arrangement_toString(int[] arrangement) {
+		ArrayList<LinkedList<Integer>> sets = new ArrayList<LinkedList<Integer>>();
+		String str = "";
+
+		for (int i = 0; i < M; ++i) {
+			sets.add(new LinkedList<Integer>());
+		}
+
+		for (int i = 0; i < N; ++i) {
+			int index = arrangement[i];
+			sets.get(index).add(numbers[i]);
+		}
+
+		Iterator<Integer> iter;
+		for (int i = 0; i < M; ++i) {
+			iter = sets.get(i).listIterator();
+			str += "{";
+			while (iter.hasNext()) {
+				int number = iter.next();
+				str += String.format("%d", number);
+				if (iter.hasNext())
+					str += ",";
+			}
+			str += "}";
+			if (i < M - 1)
+				str += ",";
+		}
+		return str;
 	}
 }
